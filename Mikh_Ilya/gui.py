e0230fb46b4800560dd9344819138a7b257d4b50
♫♣ĜaƁrĺSăTăNÏ♣♫ <55460474+TIIGR@users.noreply.github.com>
2023-11-19 19:08:46 +0300

    +

--- Mikh_Ilya/gui.py
+++ Mikh_Ilya/gui.py
@@ -38,24 +38,8 @@ with dpg.window(label="Koch's snowflake"):
     
     with dpg.drawlist(width=500, height=500, tag='drawlist'):
         
-        def coch(n):
-            if n == 0:
-                Tags.append(dpg.generate_uuid())
-                dpg.draw_line(P.toTuple(), P.sum(D).toTuple(), color=(255, 0, 0, 255), thickness=1, tag=Tags[-1])
-            else:
-                for angle in [0, 60, -120, 60]:
-                    D.turn(angle)
-                    coch(n - 1)
-        
-        level = dpg.get_value("level")
-        size = dpg.get_value("size")
-        P = Point((500 - size) / 2, 250 + size * (3) ** (1/2) / 6)
-        D = Point(size / (3 ** level), 0)
         Tags = []
-        
-        for i in range(3):
-            coch(level)
-            D.turn(-120)
+        update()
 
 dpg.show_viewport()
 dpg.start_dearpygui()
