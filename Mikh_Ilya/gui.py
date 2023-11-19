9db1f16a5ab70f398e6ca5ef61fbcc4406a6b2d1
♫♣ĜaƁrĺSăTăNÏ♣♫ <55460474+TIIGR@users.noreply.github.com>
2023-11-19 19:23:00 +0300

    +

--- Mikh_Ilya/gui.py
+++ Mikh_Ilya/gui.py
@@ -12,7 +12,9 @@ def update():
     def coch(n):
         if n == 0:
             Tags.append(dpg.generate_uuid())
-            dpg.draw_line(P.toTuple(), P.sum(D).toTuple(), color=(255, 0, 0, 255), thickness=1, parent='drawlist', tag=Tags[-1])
+            dpg.draw_line(P.toTuple(), P.sum(D).toTuple(),
+                          color=(dpg.get_value("R"), dpg.get_value("G"), dpg.get_value("B"), 255),
+                          thickness=dpg.get_value("thickness"), parent='drawlist', tag=Tags[-1])
         else:
             for angle in [0, 60, -120, 60]:
                 D.turn(angle)
@@ -35,6 +37,10 @@ with dpg.window(label="Koch's snowflake"):
     
     dpg.add_slider_int(label="Level", min_value=0, max_value=4, default_value=3, tag="level", callback=update)
     dpg.add_slider_float(label="Size", min_value=50, max_value=400, default_value=300, tag="size", callback=update)
+    dpg.add_slider_float(label="Thickness", min_value=1, max_value=5, default_value=1, tag="thickness", callback=update)
+    dpg.add_slider_float(label="Red", min_value=0, max_value=255, default_value=255, tag="R", callback=update)
+    dpg.add_slider_float(label="Green", min_value=0, max_value=255, default_value=0, tag="G", callback=update)
+    dpg.add_slider_float(label="Blue", min_value=0, max_value=255, default_value=0, tag="B", callback=update)
     
     with dpg.drawlist(width=500, height=500, tag='drawlist'):
         
