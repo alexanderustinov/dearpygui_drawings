ee3c5b8f2f467a43aadd54244db643caae3859ad
Grigory Shanin <gregoryshanin.job@gmail.com>
2023-12-01 15:04:56 +0300

    Add FPS and render loop

--- shanin/fractal/__main__.py
+++ shanin/fractal/__main__.py
@@ -1,5 +1,9 @@
 import dearpygui.dearpygui as dpg
 import math
+from timeit import default_timer as timer
+from time import sleep
+
+start_time = timer()
 
 dpg.create_context()
 
@@ -30,14 +34,27 @@ def golden_dragon(drawlist, x1, y1, x2, y2, turn, n):
             golden_dragon(drawlist, x1, y1, px, py, True, n - 1)
             golden_dragon(drawlist, px, py, x2, y2, False, n - 1)
 
-dpg.create_viewport(title='The Golden Dragon', width=1280, height=720)
+fps_text = dpg.generate_uuid()
+dpg.create_viewport(title='The Golden Dragon', width=1280, height=720, vsync=False)
 
 with dpg.window(tag="Window"):
+    dpg.add_text("FPS: ", tag=fps_text)
     with dpg.drawlist(width=1280, height=720):
         golden_dragon(dpg.drawlist, 300, 300, 700, 200, True, 100)
-
+    
 dpg.set_primary_window("Window", True)
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.start_dearpygui()
+
+frame_count = 0
+while dpg.is_dearpygui_running():
+    dpg.render_dearpygui_frame()
+    frame_count += 1
+    elapsed_time = timer() - start_time
+    if elapsed_time > 1.0:
+        fps = frame_count / elapsed_time
+        dpg.set_value(fps_text, f"FPS: {fps:.2f}")
+        start_time = timer()
+        frame_count = 0
+
 dpg.destroy_context()
